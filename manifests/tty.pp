91171780339510e3903e2d50dbea0645977d2c93
Jordi Prats <jprats@systemadmin.es>
2016-01-07 15:13:24 +0100

    poder canviar el enable

--- manifests/tty.pp
+++ manifests/tty.pp
@@ -8,7 +8,7 @@ class audit::tty($enable='*') inherits audit::params {
 
   exec { 'afegint pam_tty_audit sshd':
     command => "sed '/pam_tty_audit.so/d' -i /etc/pam.d/sshd; echo 'session required pam_tty_audit.so enable=${enable}' >> /etc/pam.d/sshd",
-    unless  => "grep pam_tty_audit.so /etc/pam.d/sshd",
+    unless  => "grep 'session required pam_tty_audit.so enable=${enable}' /etc/pam.d/sshd",
   }
 
 
