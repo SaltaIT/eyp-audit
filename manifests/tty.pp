c29fdccffae258b87f5981cf5ee7fda22faaaf5d
Jordi Prats <jprats@systemadmin.es>
2016-06-13 19:55:48 +0200

    lint

--- manifests/tty.pp
+++ manifests/tty.pp
@@ -9,17 +9,17 @@ class audit::tty($disable=undef, $enable=['*']) inherits audit::params {
 
   if($enable!=undef)
   {
-    validate_array($enable)    
+    validate_array($enable)
   }
 
   Exec {
     path => '/sbin:/bin:/usr/sbin:/usr/bin',
   }
 
+  #    command => "sed '/pam_tty_audit.so/d' -i /etc/pam.d/sshd; echo 'session required pam_tty_audit.so enable=${enable}' >> /etc/pam.d/sshd",
+  #    unless  => "grep 'session required pam_tty_audit.so enable=${enable}' /etc/pam.d/sshd",
   exec { 'afegint pam_tty_audit sshd':
-#    command => "sed '/pam_tty_audit.so/d' -i /etc/pam.d/sshd; echo 'session required pam_tty_audit.so enable=${enable}' >> /etc/pam.d/sshd",
     command => inline_template('sed \'/pam_tty_audit.so/d\' -i /etc/pam.d/sshd; echo \'session required pam_tty_audit.so<% if defined?(@disable) %> disable=<%= @disable.join(\',\') %><% end %> enable=<%= @enable.join(\',\') %>\' >> /etc/pam.d/sshd'),
-#    unless  => "grep 'session required pam_tty_audit.so enable=${enable}' /etc/pam.d/sshd",
     unless  => inline_template('grep \'session required pam_tty_audit.so<% if defined?(@disable) %> disable=<%= @disable.join(\',\') %><% end %> enable=<%= @enable.join(\',\') %>\' /etc/pam.d/sshd'),
   }
 
